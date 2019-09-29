9ea78a498763fe300352f6b945b784dbd42f740b
aakashDeveloper <ahanda205@gmail.com>
2019-09-29 18:44:56 +0100

    a

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home.component';
+import { ProductComponent } from './products/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    // All the module that you will create or
+    // import will declare here
+    // Routing will declare here
+    imports: [
+        BrowserModule
+    ],
+
+    // All the components and pipe
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ProductComponent
+    ],
+
+    // Only in the main module
+    // Only first component
+    bootstrap: [
+        AppComponent
+    ],
+
+    // All services will declare here
+    providers: []
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+}
---
+++ firstapp/src/app/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template: `<div>
+                    <hr/>
+                    <h1>My Home Component</h1>
+                </div>`
+})
+
+export class HomeComponent {
+
+}
